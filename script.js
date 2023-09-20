1f84547faa3077a64012eb7e129ff46204bd2aad
eugeneinie <eugeneinie@outlook.com>
2023-09-20 13:19:01 +0100

    Feat: Created drop down menu

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function myFunction(x) {
+function hamburgerIcon(x) {
     x.classList.toggle("change");
 }
+
+function dropdownMenu() {
+    dropdownMenuEl.style.display = dropdownMenuEl.style.display === 'block' ? 'none' : 'block';
+}
+
+const hambIcon  = document.querySelector('.container')
+const dropdownMenuEl = document.getElementById('dropdown_menu')
+// Add an event listener to the hamburgerIcon
+hambIcon.addEventListener('click', dropdownMenu)
